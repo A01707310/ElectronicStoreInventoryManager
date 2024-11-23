7835346633724d7ce8e3c61650dabfd42d0cb1b0
A01707310 <a01707310@tec.mx>
2024-11-22 22:59:54 -0600

    Delete Cliente.cpp

--- Cliente.cpp
+++
@@ -1,9 +0,0 @@
-#include "Cliente.h"
-using namespace std;
-Cliente::Cliente(string n, string e) : nombre(n), email(e) {};
-string Cliente::getNombre(){
-    return nombre;
-}
-string Cliente::getEmail(){
-    return email;
-}
