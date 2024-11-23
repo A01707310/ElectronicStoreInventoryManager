ab172e77345f789a792d15f0bb0823088e8744f3
A01707310 <a01707310@tec.mx>
2024-11-22 23:00:02 -0600

    Delete Electronico.cpp

--- Electronico.cpp
+++
@@ -1,9 +0,0 @@
-#include "Electronico.h"
-
-Electronico::Electronico(std::string n, double p) : nombre(n), precio(p) {}
-string Electronico::getNombre(){
-    return nombre;
-}
-double Electronico::getPrecio(){
-    return precio;
-}
