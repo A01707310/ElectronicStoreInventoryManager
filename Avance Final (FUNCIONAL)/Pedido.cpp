34d5356963e7fed027f1b7576e11d441945f5930
A01707310 <a01707310@tec.mx>
2024-11-25 11:57:51 -0600

    Delete Avance Final (FUNCIONAL) directory

--- Avance Final (FUNCIONAL)/Pedido.cpp
+++
@@ -1,9 +0,0 @@
-#include "Pedido.h"
-
-Pedido::Pedido(Cliente c, Factura f) : nuevoCliente(c), factura(f) {}
-Pedido::Pedido() : nuevoCliente(Cliente("", "")), factura(Factura(0.0)) {}
-
-void Pedido::agregarProducto(Electronico e) {
-    productos.push_back(e);
-    factura.setTotal(factura.getTotal()+e.getPrecio());
-}
--- Avance Final (FUNCIONAL)/Periferico.cpp
+++
@@ -1,4 +0,0 @@
-#include "Periferico.h"
-
-Periferico::Periferico(std::string n, double p, bool inalambrico) 
-    : Electronico(n, p), esInalambrico(inalambrico) {}
